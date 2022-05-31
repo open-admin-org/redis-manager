e7d831f25fe4f26c64baeec807240f53029032d7
sb | open-admin.org <info@open-admin.org>
2022-05-31 13:40:49 +0200

    init

---
+++ README.md
@@ -0,0 +1,25 @@
+Redis manager for open-admin
+========================
+
+[![StyleCI](https://styleci.io/repos/129332701/shield?branch=master)](https://styleci.io/repos/129332701)
+[![Packagist](https://img.shields.io/packagist/l/open-admin-ext/redis-manager.svg?maxAge=2592000)](https://packagist.org/packages/open-admin-ext/redis-manager)
+[![Total Downloads](https://img.shields.io/packagist/dt/open-admin-ext/redis-manager.svg?style=flat-square)](https://packagist.org/packages/open-admin-ext/redis-manager)
+[![Pull request welcome](https://img.shields.io/badge/pr-welcome-green.svg?style=flat-square)]()
+
+## Screenshot
+
+![wx20180413-100502](https://user-images.githubusercontent.com/1479100/38713113-23d561a2-3f02-11e8-9a24-9ecba5f1ea17.png)
+
+## Installation
+
+```
+$ composer require open-admin-ext/redis-manager
+
+$ php artisan admin:import redis-manager
+```
+
+Open `http://your-host/admin/redis` in your browser.
+
+License
+------------
+Licensed under [The MIT License (MIT)](LICENSE).
---
+++ src/BootExtension.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace OpenAdmin\Admin\RedisManager;
+
+use OpenAdmin\Admin\Facades\Admin;
+
+trait BootExtension
+{
+    public static function boot()
+    {
+        static::registerRoutes();
+
+        Admin::extend('redis-manager', __CLASS__);
+    }
+
+    /**
+     * Register routes for open-admin.
+     *
+     * @return void
+     */
+    protected static function registerRoutes()
+    {
+        parent::routes(function ($router) {
+            /* @var \Illuminate\Routing\Router $router */
+            $router->get('redis', 'OpenAdmin\Admin\RedisManager\RedisController@index')->name('redis-index');
+            $router->delete('redis/key', 'OpenAdmin\Admin\RedisManager\RedisController@destroy')->name('redis-key-delete');
+            $router->get('redis/fetch', 'OpenAdmin\Admin\RedisManager\RedisController@fetch')->name('redis-fetch-key');
+            $router->get('redis/create', 'OpenAdmin\Admin\RedisManager\RedisController@create')->name('redis-create-key');
+            $router->post('redis/store', 'OpenAdmin\Admin\RedisManager\RedisController@store')->name('redis-store-key');
+            $router->get('redis/edit', 'OpenAdmin\Admin\RedisManager\RedisController@edit')->name('redis-edit-key');
+            $router->put('redis/key', 'OpenAdmin\Admin\RedisManager\RedisController@update')->name('redis-update-key');
+            $router->delete('redis/item', 'OpenAdmin\Admin\RedisManager\RedisController@remove')->name('redis-remove-item');
+
+            $router->get('redis/console', 'OpenAdmin\Admin\RedisManager\RedisController@console')->name('redis-console');
+            $router->post('redis/console', 'OpenAdmin\Admin\RedisManager\RedisController@execute')->name('redis-execute');
+        });
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function import()
+    {
+        parent::createMenu('Redis manager', 'redis', 'fa-database');
+
+        parent::createPermission('Redis Manager', 'ext.redis-manager', 'redis*');
+    }
+}
